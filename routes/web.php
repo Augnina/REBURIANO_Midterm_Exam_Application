b19bec435069f8b11228940ccbda397d4acb62cd
Augnina <apr1299@students.uc-bcf.edu.ph>
2025-02-24 23:26:05 +0800

    Update web.php
    Changed routes for views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', [BooksController::class, 'index']);
+
